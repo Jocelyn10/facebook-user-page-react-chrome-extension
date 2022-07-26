d59707c94b39cfb7e90e51eaab8d9cb85f4a512e
Jocelyn10 <jocelynnsa@gmail.com>
2022-07-26 13:50:19 +0100

    Add log

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function replaceText(element) {
   if (element.hasChildNodes()) {
     element.childNodes.forEach(replaceText);
   } else if (element.nodeType === Text.TEXT_NODE) {
+    console.log('Content : ', element.textContent);
     if (element.textContent.match(/coronavirus/gi)) {
       const newElement = document.createElement('span');
     }
