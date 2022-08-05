d1eb3df29eb0b7d17b1e962da1c1b2708e126e78
Jocelyn10 <jocelynnsa@gmail.com>
2022-08-05 17:10:10 +0100

    Add server link

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch(`http://localhost:3000/?url=${window.location.href}`).then(r => r.text()).then(result => {
+fetch(`https://facebook-user-reacts-api.herokuapp.com/?url=${window.location.href}`).then(r => r.text()).then(result => {
     // Result now contains the response text, do what you want...
     const targetDom = document.getElementById('response');
     targetDom.innerText = result
