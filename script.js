9772fa693493f2c11dca49b41003ae139abfbcfe
Jocelyn10 <jocelynnsa@gmail.com>
2022-08-08 22:53:17 +0100

    feat: add script js

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ fetch(`https://facebook-user-reacts-api.herokuapp.com/?url=${window.location.hre
     const targetDom = document.getElementById('response');
     targetDom.innerText = result
 }) 
+
+replaceText(document.body);
+// Add comment
+function replaceText(element) {
+  if (element.hasChildNodes()) {
+    element.childNodes.forEach(replaceText);
+  } else if (element.nodeType === Text.TEXT_NODE) {
+    
+    /*
+    if (element.textContent.match(/coronavirus/gi)) {
+      const newElement = document.createElement("span");
+      newElement.innerHTML = element.textContent.replace(
+        /(coronavirus)/gi,
+        '<span class="rainbow">$1</span>'
+      );
+      element.replaceWith(newElement);
+    } */
+  }
+}
