a88e36437a5869f27349c2dd8f4a15a15d411446
Jocelyn10 <jocelynnsa@gmail.com>
2022-08-15 23:53:59 +0100

    do some fixes

--- script.js
+++ script.js
@@ -4,15 +4,25 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
   // use `url` here inside the callback because it's asynchronous!
 });
 
-/*
-function getUsers(url){
-    fetch(`http://ec2-3-85-47-70.compute-1.amazonaws.com:3000/?url=${url}`).then(r => r.text()).then(result => {
-        // Result now contains the response text, do what you want...
-        const targetDom = document.getElementById('response');
-        targetDom.innerText = result
-    }) 
-} */
+function getUsers(url) {
+  let textLoadingDom = document.getElementById('loading-text');
+  let loadingDom = document.getElementById('loader');
 
+  textLoadingDom.style.display = 'block';
+  loadingDom.style.display = 'block';
+
+  fetch(`http://ec2-3-85-47-70.compute-1.amazonaws.com:3000/?url=${url}`)
+    .then((r) => r.text())
+    .then((result) => {
+      // Result now contains the response text, do what you want...
+      let targetDom = document.getElementById('response');
+      textLoadingDom.style.display = 'none';
+      loadingDom.style.display = 'none';
+      targetDom.innerText = result;
+    });
+}
+
+/*
 function getUsers(url) {
   let textLoadingDom = document.getElementById('loading-text');
   let loadingDom = document.getElementsByClassName('loading');
@@ -30,6 +40,7 @@ function getUsers(url) {
       targetDom.innerText = result;
     });
 }
+*/
 
 /*
 fetch(`https://facebook-user-reacts-api.herokuapp.com/?url=${window.location.href}`).then(r => r.text()).then(result => {
